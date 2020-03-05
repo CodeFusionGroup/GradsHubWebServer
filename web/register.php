e49f7158e6efb0549218c42bdbb1145ab4999c20
tb0se <rachiditbose@gmail.com>
2020-03-05 18:14:39 +0200

    Add register

---
+++ web/register.php
@@ -0,0 +1,57 @@
+<?php>
+
+$username = "b6febc76a325a3";
+$password = "a4831502";
+$database = "heroku_6b7ffb41be0156e";
+$host = "us-cdbr-iron-east-04.cleardb.net";
+$link = mysqli_connect($host, $username, $password, $database);
+
+
+if ($result = mysqli_prepare($link,"SELECT USER_EMAIL FROM USER WHERE USER_EMAIL=?")){
+
+	mysqli_stmt_bind_param($result,"s",$user_email);
+	$user_email = $_REQUEST["USER_EMAIL"];
+
+	mysqli_stmt_execute($result);
+	mysqli_stmt_store_result($result);
+
+    // Find out if the user email doesnt already exist in the User table
+	if(mysqli_stmt_num_rows($result) > 0){
+		$output["success"]="0";
+		$output["message"]="Email already exists!, Please use another email.";
+		echo json_encode($output);
+		mysqli_close($link);
+	}else{ // Email doesnt exist in the User table
+
+		if($stmt = mysqli_prepare($link,"INSERT INTO USER VALUES(?,?,?,?,?,?)")){
+            mysqli_stmt_bind_param($stmt,"ssssss",$user_fname, $user_lname, $hashed_password, $user_email,$user_phone_no, $user_acad_status);
+
+            $user_fname = $_REQUEST["USER_FNAME"];
+            $user_lname = $_REQUEST["USER_LNAME"];
+			$user_password = $_REQUEST["USER_PASSWORD"];
+            $user_email = $_REQUEST["USER_EMAIL"];
+            $user_phone_no = $_REQUEST["USER_PHONE_NO"];
+            $user_acad_status = $_REQUEST["USER_ACAD_STATUS"];
+            //Hash the password
+            $hashed_password = password_hash($user_password,PASSWORD_DEFAULT);
+
+            if(!isset($user_fname, $user_lname, $user_password, $user_email, $user_phone_no, $user_acad_status)){
+
+				$output["success"]="0";
+				$output["message"]="You didn't send the required values!";
+				echo json_encode($output);
+				mysqli_close($link);
+				die();
+
+            }
+            
+            mysqli_stmt_execute($stmt);
+            $output["success"]="1";
+            $output["message"]="Registration successful!";
+            echo json_encode($output);
+            mysqli_close($link);
+
+		}
+	}
+}
+?>
