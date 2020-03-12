a1985e3fb499b6a1f9966343f36b629646e6cf59
tb0se <rachiditbose@gmail.com>
2020-03-12 15:57:06 +0200

    Fixing password problem in login php

--- web/login.php
+++ web/login.php
@@ -29,7 +29,7 @@ if($result = mysqli_prepare($link, "SELECT USER_EMAIL,USER_PASSWORD FROM USER WH
 
 		//mysqli_stmt_execute($result);
 		//mysqli_stmt_store_result($result);
-		echo json_encode($result);
+		//echo json_encode($result);
 
 		$row = mysqli_fetch_assoc($result);
 		echo json_encode($row["USER_PASSWORD"]);
