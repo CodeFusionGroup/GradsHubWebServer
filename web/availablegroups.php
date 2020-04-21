770adb41017181a1626efbd155255583e62af631
tb0se <rachiditbose@gmail.com>
2020-04-21 17:19:23 +0200

    availablegroups php script

--- web/availablegroups.php
+++ web/availablegroups.php
@@ -19,7 +19,7 @@ WHERE gu.USER_ID != ? ")){
     mysqli_stmt_store_result($result);
 
     mysqli_stmt_bind_result($result,$res_groupName);
-    mysqli_stmt_fetch($query);
+    mysqli_stmt_fetch($result);
 
     // Checks if there are any available groups
     if(mysqli_stmt_num_rows($result) > 0){
