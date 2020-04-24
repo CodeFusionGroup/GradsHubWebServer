a8b67a8f34a0636a61d529b205cf7c19273b63f4
tb0se <rachiditbose@gmail.com>
2020-04-24 14:09:21 +0200

    join group php file

---
+++ web/joingroup.php
@@ -0,0 +1,53 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$link  = new mysqli($server, $username, $password, $db);
+
+
+// First check if user already a memember of the group
+
+if($result = mysqli_prepare($link,"SELECT * FROM group_user WHERE USER_ID = ? AND RESEARCH_GROUP_ID = ?")){
+    
+    mysqli_stmt_bind_param($result,"ii",$user_id,$group_id);
+    $user_id = $_REQUEST["USER_ID"];
+    $group_id = $_REQUEST["RESEARCH_GROUP_ID"];
+
+    mysqli_stmt_execute($result);
+    mysqli_stmt_store_result($result);
+
+    mysqli_stmt_bind_result($result,$res_userID,$res_groupID);
+    mysqli_stmt_fetch($result);
+
+    if(mysqli_stmt_num_rows($result) > 0){
+        $output["success"] = "0";
+		$output["message"] = "You have already joined this group";
+		echo json_encode($output);
+		mysqli_close($link);
+    }else{
+
+        $stmnt = "INSERT INTO group_user(USER_ID,RESEARCH_GROUP_ID) VALUES ($res_userID,$res_groupID)";
+        if($query = mysqli_query($link,$stmnt)){
+
+            // Execute the statement i.e enter record into the table
+            mysqli_stmt_execute($stmt);
+            // Successful
+            $output["success"]="1";
+            $output["message"]="Successfully joined group";
+            echo json_encode($output);
+            mysqli_close($link);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
