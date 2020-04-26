825a761d3792c60efd07aac406e3c3afadfe0d64
tb0se <rachiditbose@gmail.com>
2020-04-26 20:44:55 +0200

    implement group code functionality

--- web/joingroup.php
+++ web/joingroup.php
@@ -26,20 +26,75 @@ if($result = mysqli_prepare($link,"SELECT * FROM group_user WHERE USER_ID = ? AN
         
     }else{// User is not a member of the group i.e record doesnt exist in group_user table
 
-        $stmnt = "INSERT INTO group_user(USER_ID,GROUP_ID) VALUES (?,?)";
-        if($query = mysqli_prepare($link,$stmnt)){
+        // init group visibility
+        $group_visib = $_REQUEST["USER_ID"];
 
-            mysqli_stmt_bind_param($query,"ii",$user_id,$group_id);
-            $user_id = $_REQUEST["USER_ID"];
-            $group_id = $_REQUEST["GROUP_ID"];
+        // Check if the group the user wants to join is private or public
+        if($group_visib == "public"){
 
-            mysqli_stmt_execute($query);
+            $stmnt = "INSERT INTO group_user(USER_ID,GROUP_ID) VALUES (?,?)";
+            if($query = mysqli_prepare($link,$stmnt)){
+
+                mysqli_stmt_bind_param($query,"ii",$user_id,$group_id);
+                $user_id = $_REQUEST["USER_ID"];
+                $group_id = $_REQUEST["GROUP_ID"];
+
+                mysqli_stmt_execute($query);
+                
+                // Successful
+                $output["success"]="1";
+                $output["message"]="Successfully joined group";
+                echo json_encode($output);
+                mysqli_close($link);
+            }
+
+        }else if($group_visib == "private"){  
+
+            // init group invite code
+            $group_code = $_REQUEST["GROUP_ID"];  
+
+            // Check if group code entered is correct
+            $stmnt1 = "SELECT GROUP_CODE FROM research_group WHERE GROUP_ID = ?";
+            if($request = mysqli_prepare($link,$stmnt1)){
+
+                mysqli_stmt_bind_param($request,"i",$group_id);
+                $group_id = $_REQUEST["GROUP_ID"];
+
+                mysqli_stmt_execute($request);
+                mysqli_stmt_store_result($request);
+
+                mysqli_stmt_bind_result($request,$req_groupCode);
+                mysqli_stmt_fetch($request);
+
+                // Verify group code
+                if($req_groupCode == $group_code){
+
+                    $stmnt = "INSERT INTO group_user(USER_ID,GROUP_ID) VALUES (?,?)";
+                    if($query = mysqli_prepare($link,$stmnt)){
+        
+                        mysqli_stmt_bind_param($query,"ii",$user_id,$group_id);
+                        $user_id = $_REQUEST["USER_ID"];
+                        $group_id = $_REQUEST["GROUP_ID"];
+        
+                        mysqli_stmt_execute($query);
+                        
+                        // Successful
+                        $output["success"]="1";
+                        $output["message"]="Successfully joined group";
+                        echo json_encode($output);
+                        mysqli_close($link);
+                    }
+                    
+                }else{
+                    // Unsuccessful
+                    $output["success"]="0";
+                    $output["message"]="Incorrect invite code";
+                    echo json_encode($output);
+                    mysqli_close($link);
+                }
+
+            }
             
-            // Successful
-            $output["success"]="1";
-            $output["message"]="Successfully joined group";
-            echo json_encode($output);
-            mysqli_close($link);
         }
     }
 }
