b53be031056971b5350221ccbc01b6e17c3bae6a
tb0se <rachiditbose@gmail.com>
2020-05-25 15:00:04 +0200

    modify retrievegrouppost.php to return userFname&Lname

--- web/retrievegrouppost.php
+++ web/retrievegrouppost.php
@@ -7,7 +7,12 @@ $db = substr($url["path"], 1);
 $link  = new mysqli($server, $username, $password, $db);
 
 
-$stmnt = "SELECT POST_TITLE,POST_DATE,POST_ATTACHMENT_URL FROM group_post where GROUP_ID = ?";
+// $stmnt = "SELECT POST_TITLE,POST_DATE,POST_ATTACHMENT_URL FROM group_post where GROUP_ID = ?";
+
+$stmnt = " SELECT u.USER_FNAME, u.USER_LNAME, gp.POST_TITLE, gp.POST_DATE,gp.POST_ATTACHMENT_URL FROM group_post as gp 
+INNER JOIN group_user as gu ON gp.GROUP_USER_ID = gu.GROUP_USER_ID
+INNER JOIN user as u ON gu.USER_ID = u.USER_ID
+where gp.GROUP_ID = ? ";
 
 if ( $result = mysqli_prepare($link,$stmnt)){
 
@@ -17,7 +22,7 @@ if ( $result = mysqli_prepare($link,$stmnt)){
     mysqli_stmt_execute($result);
     mysqli_stmt_store_result($result);
 
-    mysqli_stmt_bind_result($result,$res_postTitle,$res_postDate,$res_postUrl);
+    mysqli_stmt_bind_result($result,$res_userFname,$res_userLname,$res_postTitle,$res_postDate,$res_postUrl);
     mysqli_stmt_fetch($result);
 
 
@@ -30,6 +35,8 @@ if ( $result = mysqli_prepare($link,$stmnt)){
     }else if(mysqli_stmt_num_rows($result) > 0){
 
         // Display first row item(record)
+        $group["USER_FNAME"] = $res_userFname;
+        $group["USER_LNAME"] = $res_userLname;
         $group["POST_TITLE"] = $res_postTitle;
         $group["POST_DATE"] = $res_postDate;
         $group["POST_URL"] = $res_postUrl;
@@ -37,6 +44,8 @@ if ( $result = mysqli_prepare($link,$stmnt)){
 
         // Fetch the rest of the row items(records)
         while ($result->fetch()){
+            $group["USER_FNAME"] = $res_userFname;
+            $group["USER_LNAME"] = $res_userLname;
             $group["POST_TITLE"] = $res_postTitle;
             $group["POST_DATE"] = $res_postDate;
             $group["POST_URL"] = $res_postUrl;
