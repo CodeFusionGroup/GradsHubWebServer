99b227ea1a7cfe8648bdc15104150e77857f7650
tb0se <rachiditbose@gmail.com>
2020-05-27 22:14:57 +0200

    modify retrivegroupposts to return likes,comment&ID

--- web/retrievegrouppost.php
+++ web/retrievegrouppost.php
@@ -9,10 +9,12 @@ $link  = new mysqli($server, $username, $password, $db);
 
 // $stmnt = "SELECT POST_TITLE,POST_DATE,POST_ATTACHMENT_URL FROM group_post where GROUP_ID = ?";
 
-$stmnt = " SELECT u.USER_FNAME, u.USER_LNAME, gp.POST_TITLE, gp.POST_DATE,gp.POST_ATTACHMENT_URL FROM group_post as gp 
+$stmnt = " SELECT u.USER_FNAME, u.USER_LNAME, gp.GROUP_POST_ID,gp.POST_TITLE, gp.POST_DATE,gp.POST_ATTACHMENT_URL FROM group_post as gp 
 INNER JOIN group_user as gu ON gp.GROUP_USER_ID = gu.GROUP_USER_ID
 INNER JOIN user as u ON gu.USER_ID = u.USER_ID
 where gp.GROUP_ID = ? ";
+
+
 
 if ( $result = mysqli_prepare($link,$stmnt)){
 
@@ -22,7 +24,7 @@ if ( $result = mysqli_prepare($link,$stmnt)){
     mysqli_stmt_execute($result);
     mysqli_stmt_store_result($result);
 
-    mysqli_stmt_bind_result($result,$res_userFname,$res_userLname,$res_postTitle,$res_postDate,$res_postUrl);
+    mysqli_stmt_bind_result($result,$res_userFname,$res_userLname,$res_groupPostID,$res_postTitle,$res_postDate,$res_postUrl);
     mysqli_stmt_fetch($result);
 
 
@@ -34,21 +36,64 @@ if ( $result = mysqli_prepare($link,$stmnt)){
         mysqli_close($link);
     }else if(mysqli_stmt_num_rows($result) > 0){
 
+        // find number of likes
+        $stmnt_2 = "SELECT COUNT(gpl.POST_LIKE_ID) as NO_OF_LIKES from group_post_like as gpl
+        WHERE GROUP_POST_ID = $res_groupPostID
+        GROUP BY gpl.POST_LIKE_ID";
+
+        // find number of comments
+        $stmnt_3 = "SELECT COUNT(gpc.POST_COMMENT_ID) as NO_OF_COMMENTS from group_post_comment as gpc
+        WHERE GROUP_POST_ID = $res_groupPostID
+        GROUP BY gpc.POST_COMMENT_ID";
+
+        if( ($result_2 = mysqli_query($link,$stmnt_2)) && ($result_3 = mysqli_query($link,$stmnt_2)) ){
+
+            if( (mysqli_num_rows($result_2) > 0) && (mysqli_num_rows($result_2) > 0) ){
+                // Fetch values
+                $no_likes = $result_2->fetch_assoc();
+                $no_comments = $result_3->fetch_assoc();
+            }else if ( (mysqli_num_rows($result_2) < 0) && (mysqli_num_rows($result_2) < 0) ) {
+                // 
+
+            }
+
+            // LIKES
+            if( mysqli_num_rows($result_2) > 0 ){
+                $no_likes = $result_2->fetch_assoc();
+            }else{
+                $no_likes = 0;
+            }
+
+            // COMMENTS
+            if( mysqli_num_rows($result_2) > 0 ){
+                $no_comments = $result_3->fetch_assoc();
+            }else{
+                $no_comments = 0;
+            }
+
+        }
+
         // Display first row item(record)
         $group["USER_FNAME"] = $res_userFname;
         $group["USER_LNAME"] = $res_userLname;
+        $group["POST_ID"] = $res_groupPostID;
         $group["POST_TITLE"] = $res_postTitle;
         $group["POST_DATE"] = $res_postDate;
         $group["POST_URL"] = $res_postUrl;
+        $group["NO_LIKES"] = $no_likes;
+        $group["NO_COMMENTS"] = $no_comments;
         $output[]=$group;
 
         // Fetch the rest of the row items(records)
         while ($result->fetch()){
             $group["USER_FNAME"] = $res_userFname;
             $group["USER_LNAME"] = $res_userLname;
+            $group["POST_ID"] = $res_groupPostID;
             $group["POST_TITLE"] = $res_postTitle;
             $group["POST_DATE"] = $res_postDate;
             $group["POST_URL"] = $res_postUrl;
+            $group["NO_LIKES"] = $no_likes;
+            $group["NO_COMMENTS"] = $no_comments;
             $output[]=$group;
         }
         
