2ee55e48979156425e4c25e8435773590afeedd2
tb0se <rachiditbose@gmail.com>
2020-05-29 23:11:33 +0200

    modify insertlikesGP.php

--- web/insertlikesGP.php
+++ web/insertlikesGP.php
@@ -33,7 +33,7 @@ for ( $i = 0;$i < count($post_id_arr); $i++ ){
     if( mysqli_num_rows($result_check) > 0){
 
         $display["success"] = "0";
-        $display["message"] = "You have already like this post";
+        $display["message"] = "You have already liked this post.";
         array_push($output,$display);
 
     }else{
@@ -49,7 +49,7 @@ for ( $i = 0;$i < count($post_id_arr); $i++ ){
             // $valid_count++;
 
             $display["success"] = "1";
-            $display["message"] = "Successfully like the post";
+            $display["message"] = "Successfully liked the post.";
             array_push($output,$display);
 
 
@@ -64,81 +64,5 @@ for ( $i = 0;$i < count($post_id_arr); $i++ ){
 // Output the result and close the link
 echo json_encode($output);
 mysqli_close($link);
-
-
-// if($valid_count == count($post_id_arr)){
-//     $output["success"] = "1";
-//     $output["message"] = "Successfully liked posts.";
-//     echo json_encode($output);
-//     mysqli_close($link);
-// }else{
-//     echo " Not all likes were inserted";
-// }
-
-
-
-
-// ########## ATTEMPTED BINDING&PREPARED STMNTS CODE ##########
-
-// $post_id_string = $_REQUEST["POST_ID"];
-// echo "POST_ID string: ";
-// echo json_encode($post_id_string);
-// // post_id array
-// $post_id_arr = array();
-// // Store string post_ids in an array
-// $post_id_arr = explode(',',$post_id_string);
-
-
-// // Insert likes for all posts
-// for ($i = 0 ; $i < count($post_id_arr); $i++){
-
-//     // Insert likes into database
-//     $stmnt = "INSERT INTO group_post_like (GROUP_POST_ID,GROUP_USER_ID,POST_LIKE)
-//     VALUES ( ?,(SELECT GROUP_USER_ID AS MemberNumber FROM GROUP_USER 
-//     WHERE USER_ID = ? AND GROUP_ID = ?) , true)";
-
-//     if( $result  = mysqli_prepare($link,$stmnt) ){
-        
-//         echo "POST_ID: ";
-//         echo json_encode($post_id_arr[$i]);
-
-//         mysqli_stmt_bind_param($result,"iii",$post_id,$user_id,$group_id);
-//         $post_id = $post_id_arr[$i];
-//         $user_id = $_REQUEST["USER_ID"];
-//         $group_id = $_REQUEST["GROUP_ID"];
-
-//         // Check if all the values were sent
-//         if(!isset($post_id, $user_id, $user_id)){
-//             // Unsuccessful
-//             $output["success"]="0";
-//             $output["message"]="You didn't send the required values!";
-//             echo json_encode($output);
-//             mysqli_close($link);
-//             die();
-
-//         }
-
-
-//         // Execute the query
-//         if( mysqli_stmt_execute($result) ){
-//             // Successful
-//             $output["success"]="1";
-//             $output["message"]="Successfully liked the posts";
-//             echo json_encode($output);
-//             mysqli_close($link);
-            
-//         }else{
-//             echo "Error Inserting";
-//         }
-
-        
-//     }else{
-//         echo "Error with insert statement";
-//     }
-
-
-// }
-
-
 
 ?>
