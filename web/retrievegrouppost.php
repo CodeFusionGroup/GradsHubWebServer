21d5b596a2648ca37e48cea7c0381161b17af7a0
tb0se <rachiditbose@gmail.com>
2020-06-01 16:26:04 +0200

    modify retrievegrouppost.php

--- web/retrievegrouppost.php
+++ web/retrievegrouppost.php
@@ -6,7 +6,6 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 $link  = new mysqli($server, $username, $password, $db);
 
-
 // $stmnt = " SELECT u.USER_FNAME, u.USER_LNAME, gp.GROUP_POST_ID,gp.POST_TITLE, gp.POST_DATE,gp.POST_ATTACHMENT_URL,
 // COUNT(gpc.POST_COMMENT_ID) as NO_OF_COMMENTS,COUNT(gpl.POST_LIKE_ID) as NO_OF_LIKES 
 // FROM group_post as gp 
@@ -81,17 +80,8 @@ if($query_post && $query_count){
         // Combine everything into one array
         for($i = 0 ;$i < count($full_post_arr); $i++ ){
 
-            echo json_encode($post_counts_arr[$i]["NO_OF_COMMENTS"]);
             $full_post_arr[$i]["NO_OF_COMMENTS"] = $post_counts_arr[$i]["NO_OF_COMMENTS"];
             $full_post_arr[$i]["NO_OF_LIKES"] = $post_counts_arr[$i]["NO_OF_LIKES"];
-
-            // push info into full array
-            // array_push($full_post_arr,$post_info_arr[$i]);
-
-            // // push counts into full array
-            // array_push($full_post_arr,$post_counts_arr[$i]);
-
-
 
         }
 
@@ -107,61 +97,4 @@ if($query_post && $query_count){
     echo "Error with the queries.";
 }
 
-
 ?>
-
-<!-- OLD BINDING AND PREPARED SOLUTION -->
-
-<!-- if ( $result = mysqli_prepare($link,$stmnt)){
-
-mysqli_stmt_bind_param($result,"i",$group_id);
-$group_id = $_REQUEST["GROUP_ID"];
-
-mysqli_stmt_execute($result);
-mysqli_stmt_store_result($result);
-
-mysqli_stmt_bind_result($result,$res_userFname,$res_userLname,$res_groupPostID,$res_postTitle,$res_postDate
-,$res_postUrl,$res_noComments,$res_noLikes);
-mysqli_stmt_fetch($result);
-
-
-if(mysqli_stmt_num_rows($result) == 0){
-    // Unsuccessful
-    $output["success"] = "0";
-    $output["message"] = "This group has no posts yet.";
-    echo json_encode($output);
-    mysqli_close($link);
-}else if(mysqli_stmt_num_rows($result) > 0){
-
-    // Display first row item(record)
-    $group["USER_FNAME"] = $res_userFname;
-    $group["USER_LNAME"] = $res_userLname;
-    $group["POST_ID"] = $res_groupPostID;
-    $group["POST_TITLE"] = $res_postTitle;
-    $group["POST_DATE"] = $res_postDate;
-    $group["POST_URL"] = $res_postUrl;
-    $group["NO_COMMENTS"] = $res_noComments;
-    $group["NO_LIKES"] = $res_noLikes;
-    $output[]=$group;
-
-    // Fetch the rest of the row items(records)
-    while ($result->fetch()){
-        $group["USER_FNAME"] = $res_userFname;
-        $group["USER_LNAME"] = $res_userLname;
-        $group["POST_ID"] = $res_groupPostID;
-        $group["POST_TITLE"] = $res_postTitle;
-        $group["POST_DATE"] = $res_postDate;
-        $group["POST_URL"] = $res_postUrl;
-        $group["NO_COMMENTS"] = $res_noComments;
-        $group["NO_LIKES"] = $res_noLikes;;
-        $output[]=$group;
-    }
-    
-    // Successful
-    $display["success"] = "1";
-    $display["message"] = $output;
-    echo json_encode($display);
-    mysqli_close($link);
-
-}
-} -->
