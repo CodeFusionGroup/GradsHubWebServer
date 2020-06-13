4f3748e98a6dcc8f637330f93eacf8b6f776146d
tb0se <rachiditbose@gmail.com>
2020-06-13 15:24:13 +0200

    reorder group post comments

--- web/retrievecommentsGP.php
+++ web/retrievecommentsGP.php
@@ -16,7 +16,8 @@ $post_comments = array();
 $stmnt = "SELECT u.USER_FNAME,u.USER_LNAME,gpc.POST_COMMENT,gpc.POST_COMMENT_DATE FROM group_post_comment AS gpc
 INNER JOIN group_user AS gu ON gpc.GROUP_USER_ID = gu.GROUP_USER_ID
 INNER JOIN user AS u ON gu.USER_ID = u.USER_ID
-WHERE gpc.GROUP_POST_ID = $post_id";
+WHERE gpc.GROUP_POST_ID = $post_id
+ORDER BY gpc.POST_COMMENT_DATE DESC";
 
 if( $result = mysqli_query($link,$stmnt) ){
 
