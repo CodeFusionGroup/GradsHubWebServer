d1612ed7ebafe058666bf15507a50fe13827aa73
sizxle <61417360+sizxle@users.noreply.github.com>
2020-09-25 16:16:58 +0200

    Update updateUserProfile.php

--- web/api/User/updateUserProfile.php
+++ web/api/User/updateUserProfile.php
@@ -17,10 +17,10 @@
     $data = json_decode(file_get_contents("php://input"));
 
     // Make sure data is not empty
-    if(isset($data->user_name,$data->password
+    if(isset($data->user_name,
         $data->email,$data->phone_no,$data->academic_status,$data->profile_picture)){
 
-            // Ensure User does not already exist
+            // Ensure EMAIL does not already exist
             $user_query = $user_obj->getUserByEmail($data->email);
             $count_user_query = $user_query->rowCount();
             if($count_user_query == 0 ){
@@ -55,10 +55,38 @@
                 
                 
             }else{
+                
                 //User exists
                 $output["success"]="-1";
                 $output["message"]="Email already exists, please use another email.";
-                echo json_encode($output);
+                //echo json_encode($output);
+
+                if(strlen($data->phone_no) == 10){
+
+                    // Set the user property values
+                    $user_obj->user_name = $data->user_name;
+                    $hashed_password = password_hash($data->password,PASSWORD_DEFAULT);
+                    $user_obj->password = $hashed_password;
+                    $user_obj->email = $data->email;
+                    $user_obj->phone_no = $data->phone_no;
+                    $user_obj->acad_status = $data->academic_status;
+                    $user_obj->profile_picture = $data->profile_picture;
+                    
+
+                    // update user details
+                    if($user_obj->updateUserProfile($data->user_id)){
+                        $output["success"]="1";
+                        $output["message"]="Update successful!";
+                        echo json_encode($output);
+                    } else{
+                        echo 'could not update profile.';
+                    }
+                }else{
+                    $output["success"]="-1";
+                    $output["message"]="Incorrect Phone Number length";
+                    echo json_encode($output);
+                }
+
             }
 
     }else{
