8ac7752bac613a6ab1eda2cf47511a704d5f8a8b
tb0se <rachiditbose@gmail.com>
2020-10-05 13:04:00 +0200

    init blocked user message retreival

--- web/api/Chat/retrievemessages.php
+++ web/api/Chat/retrievemessages.php
@@ -9,9 +9,13 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/config/vars.php';
     // Get the classes
     include_once $_SERVER['DOCUMENT_ROOT'] . '/class/chat.php';
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/class/blocked.php';
 
-    // Create Message object
+    // Create Chat object
     $chat_obj = new Chat();
+
+    // Create Blocked object
+    $blocked_obj = new Blocked();
 
     // Get the posted data
     $data = json_decode(file_get_contents("php://input"));
@@ -26,29 +30,59 @@
         // Check if the chat exists
         if( $chat_obj->chatExist($chat_name1,$chat_name2) ){
             
-            $stmnt = $chat_obj->getMessages();
-            $stmnt_count = $stmnt->rowCount();
+            // Check whether current user has blocked the other user
+            if($blocked_obj->checkBlocked($data->user_id_one,$data->user_id_two)){
+                // Current user has blocked other user so only recieve messages before being blocked
 
-            
+                $stmnt = $chat_obj->getMessagesBlocked($data->user_id_one,$data->user_id_two);
+                $stmnt_count = $stmnt->rowCount();
 
-            if($stmnt_count>0){
+                if($stmnt_count>0){
 
-                //Output
-                $output["success"] = "1";
-                $messages_arr = array();
+                    //Output
+                    $output["success"] = "1";
+                    $messages_arr = array();
 
-                while($row = $stmnt->fetch(PDO::FETCH_ASSOC) ){
-                    extract($row);
-                    array_push($messages_arr,$row);
+                    while($row = $stmnt->fetch(PDO::FETCH_ASSOC) ){
+                        extract($row);
+                        array_push($messages_arr,$row);
+                    }
+
+                    $output["message"] = $messages_arr;
+                    echo json_encode($output);
+
+                }else{
+                    $output["success"] = "0";
+                    $output["message"] = "No Messages";
+                    echo json_encode($output);
                 }
 
-                $output["message"] = $messages_arr;
-                echo json_encode($output);
 
             }else{
-                $output["success"] = "0";
-                $output["message"] = "No Messages";
-                echo json_encode($output);
+                // Current user is not blocked so retrieve all messages
+
+                $stmnt = $chat_obj->getMessages();
+                $stmnt_count = $stmnt->rowCount();
+
+                if($stmnt_count>0){
+
+                    //Output
+                    $output["success"] = "1";
+                    $messages_arr = array();
+
+                    while($row = $stmnt->fetch(PDO::FETCH_ASSOC) ){
+                        extract($row);
+                        array_push($messages_arr,$row);
+                    }
+
+                    $output["message"] = $messages_arr;
+                    echo json_encode($output);
+
+                }else{
+                    $output["success"] = "0";
+                    $output["message"] = "No Messages";
+                    echo json_encode($output);
+                }
             }
 
         }else{
