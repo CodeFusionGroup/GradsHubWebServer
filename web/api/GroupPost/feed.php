198820badd8d2abb225f763375346432d98f5c09
tb0se <rachiditbose@gmail.com>
2020-10-12 14:42:38 +0200

    minor bugs

--- web/api/GroupPost/feed.php
+++ web/api/GroupPost/feed.php
@@ -51,6 +51,7 @@
 
             // $full_post_arr[$i]["NO_OF_COMMENTS"] = $post_counts_arr[$i]["NO_OF_COMMENTS"];
             // $full_post_arr[$i]["NO_OF_LIKES"] = $post_counts_arr[$i]["NO_OF_LIKES"];
+            
             $full_post_arr[$i]["NO_OF_COMMENTS"] = isset($post_counts_arr[$i]["NO_OF_COMMENTS"]) ? $post_counts_arr[$i]["NO_OF_COMMENTS"]: null;
             $full_post_arr[$i]["NO_OF_LIKES"] = isset($post_counts_arr[$i]["NO_OF_LIKES"]) ? $post_counts_arr[$i]["NO_OF_LIKES"]:null;
 
